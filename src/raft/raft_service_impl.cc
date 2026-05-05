a83d776ade42f0e6f2868830e8166e0e36168d9a
Daniel Liu <danielliu@Daniels-MacBook-Air.local>
2026-05-05 10:43:19 -0400

    implement RequestVote and AppendEntries in RaftServiceImpl

--- src/raft/raft_service_impl.cc
+++ src/raft/raft_service_impl.cc
@@ -3,9 +3,13 @@
 RaftServiceImpl::RaftServiceImpl(RaftNode& raft_node) : raft_node{raft_node}{}
 
 grpc::Status RaftServiceImpl::RequestVote(grpc::ServerContext*, const raft::VoteRequest* req, raft::VoteResponse* resp) {
+    bool granted = raft_node.request_vote(req->term(), req->candidate_id());
+    resp->set_vote_granted(granted);
     return grpc::Status::OK;
 }
 
 grpc::Status RaftServiceImpl::AppendEntries(grpc::ServerContext*, const raft::AppendEntriesRequest* req, raft::AppendEntriesResponse* resp) {
+    bool success = raft_node.append_entries(req->term(), req->leader_id());
+    resp->set_success(success);
     return grpc::Status::OK;
 }
