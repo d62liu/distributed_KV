00459ba071a07875bbf068f7ca50fa1816b1c5d8
Daniel Liu <danielliu@Daniels-MacBook-Air.local>
2026-05-05 11:54:54 -0400

    implement log replication in RaftNode: propose, apply_committed, updated append_entries and step_down

--- src/raft/raft_node.h
+++ src/raft/raft_node.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <string>
 #include <vector>
-#include <functional>
 #include <cstdint>
+#include <optional>
 #include "util/timer.h"
+#include "raft_service.pb.h"
+#include "kv_service.pb.h"
+#include "storage/store.h"
 
 enum class State {
     Follower,
@@ -15,24 +18,25 @@ class RaftNode {
     std::string node_id;
     std::string partition_id;
     std::vector<std::string> peers;
+    Store& store;
     State state;
     Timer timer;
+    std::optional<Timer> heart_beat;
     std::string voted_for;
     unsigned term_number;
+    std::vector<raft::LogEntry> log;
+    uint64_t commit_index;
+    uint64_t last_applied;
 
 public:
-    RaftNode(std::string node_id, std::string partition_id, std::vector<std::string> peers)
-        : node_id(std::move(node_id))
-        ,partition_id(std::move(partition_id))
-        ,peers(std::move(peers))
-        ,state(State::Follower)
-        ,timer([this]() { start_election(); })
-        ,term_number(0)
-        ,voted_for("")
-    {}
+    RaftNode(std::string node_id, std::string partition_id, std::vector<std::string> peers, Store& store);
     void start_election();
     bool request_vote(uint64_t term, const std::string& candidate_id);
-    bool append_entries(uint64_t term, const std::string& leader_id);
+    bool append_entries(const raft::AppendEntriesRequest& req);
+    bool propose(const Command& cmd);
     void send_heartbeat();
 
+private:
+    void step_down(uint64_t term);
+    void apply_committed();
 };
