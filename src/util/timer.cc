39a48c6b2ba138f26415121132f366feb5aa24ff
Daniel Liu <danielliu@Daniels-MacBook-Air.local>
2026-05-05 10:43:14 -0400

    add fixed-duration constructor overload to Timer for heartbeat use

--- src/util/timer.cc
+++ src/util/timer.cc
@@ -14,10 +14,15 @@ Timer::Timer(std::function<void()> callback)
     thread = std::thread(&Timer::run, this);
 }
 
+Timer::Timer(uint64_t time_ms, std::function<void()> callback)
+    : callback(std::move(callback))
+    , duration(time_ms) {
+    thread = std::thread(&Timer::run, this);
+    }
+
 void Timer::run() {
     while (true) {
         std::unique_lock<std::mutex> lock(mu);
-        duration = random_duration();
         cv.wait_for(lock, duration, [this]() { return stopped || reset_requested; });
         if (stopped) return;
         if (reset_requested) {
