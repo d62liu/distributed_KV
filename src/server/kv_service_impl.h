006d20871e6f5845dae89a0caa62702521752c09
Daniel Liu <danielliu@Daniels-MacBook-Air.local>
2026-05-05 11:55:07 -0400

    route KV writes through Raft consensus via RaftNode::propose

--- src/server/kv_service_impl.h
+++ src/server/kv_service_impl.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "kv_service.grpc.pb.h"
 #include "storage/store.h"
+#include "raft/raft_node.h"
 
 class KVServiceImpl : public KVService::Service {
 private:
     Store& store;
+    RaftNode& raft_node;
 public:
-    KVServiceImpl(Store& store);
+    KVServiceImpl(Store& store, RaftNode& raft_node);
     grpc::Status Put(grpc::ServerContext*, const PutRequest*, PutResponse*) override;
     grpc::Status Get(grpc::ServerContext*, const GetRequest*, GetResponse*) override;
     grpc::Status Delete(grpc::ServerContext*, const DeleteRequest*, DeleteResponse*) override;
